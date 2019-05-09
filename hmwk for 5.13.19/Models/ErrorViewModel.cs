b9dce3b4c42ceed97989d6ea47c73a22d18c8714
Chana <chanakarmel@gmail.com>
2019-05-08 22:07:26 -0400

    Add project files.

---
+++ hmwk for 5.13.19/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace hmwk_for_5._13._19.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
